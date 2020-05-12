3e67ffa76988845fea942beb1fc45f182fd3410e
Jefferson Candido <jefferson.ufu@gmail.com>
2020-05-11 23:36:55 -0300

    Criação da rota de listagem de transações e saldo

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,20 +1,24 @@
 import { Router } from 'express';
+import { getCustomRepository, getRepository } from 'typeorm';
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+  const transactions = await transactionsRepository.find();
+  const balance = await transactionsRepository.getBalance();
+
+  return response.json({ transactions, balance });
 });
 
 transactionsRouter.post('/', async (request, response) => {
   const { title, value, type, category } = request.body;
-
-  console.log(`Request Body: ${title}, ${value}, ${type}, ${category}`);
 
   const createTransaction = new CreateTransactionService();
 
